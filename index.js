676d969f37581216bcda13b45ecef7e57eddced2
amrit180 <amritanshusingh1806@gmail.com>
2021-12-22 21:07:48 +0530

    auth admin check

---
+++ index.js
@@ -0,0 +1,27 @@
+// authentication check for firebase user
+
+const authenFBCheck = (option) => {
+  try {
+    const firebaseUser = await admin.auth().verifyIdToken(option.authtoken);
+    return firebaseUser;
+  } catch (error) {
+    res.status(401).json({
+      err: "Invalid or expired token",
+    });
+  }
+};
+
+// role check for admin
+const adminCheck = (option) => {
+  if (option.role !== "admin") {
+    res.status(403).json({
+      err: "Admin resource. Access denied.",
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports.authenFBCheck = authenFBCheck;
+
+module.exports.adminCheck = adminCheck;
